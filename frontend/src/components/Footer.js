9ad3d4c7d246e37c2bc6e952997a716b9d8ae885
pasan1227 <pasanratnayake@gmail.com>
2022-02-18 00:49:44 +0530

    Footer changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center' py-3>
-            Copyright &copy; Softgenix
+          <Col className='text-center py-4'>
+            Copyright &copy; Sri Lankan Unique Rides
           </Col>
         </Row>
       </Container>
